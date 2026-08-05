d94ec13c4ddd34957183a798d1d0a10828e8ead8
phatemeh amiri <fatemehamiri978@gmail.com>
2026-08-05 20:30:25 +0330

    add features/home structure

---
+++ src/features/home/FeaturedRestaurants.tsx
@@ -0,0 +1,3 @@
+export default function FeaturedRetaurant() {
+    return <section>Restaurant</section>;
+  }
---
+++ src/features/home/NewsLetter.tsx
@@ -0,0 +1,3 @@
+export default function NewsLetter() {
+    return <section>NewLetter</section>;
+  }
