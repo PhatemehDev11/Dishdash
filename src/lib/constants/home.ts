07cfab7142290768362b076a7c3d08200161b55f
phatemeh amiri <fatemehamiri978@gmail.com>
2026-08-07 19:04:13 +0330

    add popular dishes data

--- src/lib/constants/home.ts
+++ src/lib/constants/home.ts
@@ -33,3 +33,36 @@ import {
       icon: IceCreamBowl,
     },
   ];
+
+
+
+export const POPULAR_DISHES = [
+  {
+    category: "ITALIAN",
+    name: "Spicy Arrabbiata",
+    price: "$18.00",
+    image: "",
+    color: "bg-rose-100",
+  },
+  {
+    category: "MEDITERRANEAN",
+    name: "Falafel Wrap",
+    price: "$14.50",
+    image: "",
+    color: "bg-emerald-100",
+  },
+  {
+    category: "THAI",
+    name: "Yellow Curry Bowl",
+    price: "$16.00",
+    image: "",
+    color: "bg-yellow-100",
+  },
+  {
+    category: "JAPANESE",
+    name: "Salmon Nigiri Set",
+    price: "$23.00",
+    image: "",
+    color: "bg-blue-100",
+  },
+];
